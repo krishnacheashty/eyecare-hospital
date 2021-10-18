ff7e2ee57678d64a140b5542255079df35059e4b
krishnaCheashty <krishnacheashty@gmail.com>
2021-10-18 18:06:29 +0600

    useauth complite

--- src/Pages/hooks/useFirebase.js
+++ src/Pages/hooks/useFirebase.js
@@ -1,7 +1,32 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
 import { useState } from "react";
+import initializeAuthentication from "../firebase/initializeAuthen";
 
+initializeAuthentication();
 const useFirebase=()=>{
-    const[user,setuser]=useState('')
+    const[user,setUser]=useState({})
+    const [email,setEmail]=useState('')
+    const[password,setPassword]=useState('')
+
     const auth=getAuth();
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((result)=>{
+        const user=result.user
+        setUser(email,password)
+    })
+    .catch((error)=>{
+        const errorMassage=error.massage
+    })
+    signInWithEmailAndPassword(auth, email ,password)
+    .then((result)=>{
+        const user=result.user
+    })
+    return{
+        user,
+
+    }
 }
+
+
+export default useFirebase;
